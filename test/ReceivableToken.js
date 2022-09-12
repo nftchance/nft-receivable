6f864639c38596ca81841679c4ea1561ce8151cd
nftchance <chance@utc24.io>
2022-09-11 23:11:05 -0500

    feat: erc20 support

--- test/ReceivableToken.js
+++ test/ReceivableToken.js
@@ -6,7 +6,7 @@ var chai = require('chai')
 
 const { ethers } = require("hardhat");
 
-describe("Jackpot", function () {
+describe("Receivable", function () {
     before(async () => {
         [owner, address1] = await ethers.getSigners();
 
@@ -26,7 +26,7 @@ describe("Jackpot", function () {
 
     describe('Token Deployment', async () => {
         it('ReceivableToken successfully.', async () => {
-            const address = receivableToken.address 
+            const address = receivableToken.address
             assert.notEqual(address, '')
             assert.notEqual(address, 0x0)
             assert.notEqual(address, null)
@@ -34,37 +34,37 @@ describe("Jackpot", function () {
         })
     });
 
-    describe("ETH Payments", async () => { 
+    describe("ETH Payments", async () => {
         it("Minting 10 tokens", async () => {
             await receivableToken.mintToken({
                 tokenType: 0,
                 tokenAddress: "0x0000000000000000000000000000000000000000",
                 tokenId: 0,
                 aux: 0
-            }, {value: ethers.utils.parseEther('0.2')});
+            }, { value: ethers.utils.parseEther('0.2') });
             const balance = await receivableToken.balanceOf(owner.address)
             assert.equal(balance.toString(), 10)
         })
 
-        it("Minting 10 tokens by transferring ETH value to contract.", async () => { 
+        it("Minting 10 tokens by transferring ETH value to contract.", async () => {
             balance = await receivableToken.balanceOf(owner.address)
             assert.equal(balance.toString(), 10)
-            await owner.sendTransaction({to: receivableToken.address, value: ethers.utils.parseEther('0.2')});
+            await owner.sendTransaction({ to: receivableToken.address, value: ethers.utils.parseEther('0.2') });
             balance = await receivableToken.balanceOf(owner.address)
             assert.equal(balance.toString(), 20)
         })
     })
 
     describe("1155 Payments", async () => {
-        before(async () => { 
+        before(async () => {
             // Deploy mock 1155 that can be used as payment
             const MockERC1155 = await ethers.getContractFactory("MockERC1155");
             mockERC1155 = await MockERC1155.deploy("ipfs://");
             mockERC1155 = await mockERC1155.deployed();
-            
+
             // Mint 10 tokens to address 1
-            await mockERC1155.connect(address1).mint(address1.address, 0, 10, "0x");
-            assert.equal((await mockERC1155.balanceOf(address1.address, 0)).toString(), 10)
+            await mockERC1155.connect(address1).mint(address1.address, 0, 1, "0x");
+            assert.equal((await mockERC1155.balanceOf(address1.address, 0)).toString(), 1)
 
             // Deploy receivable token
             receivable1155 = await ReceivableToken.deploy(
@@ -80,21 +80,21 @@ describe("Jackpot", function () {
             receivable1155 = await receivable1155.deployed();
         })
 
-        it("Can mint 1 with the transfer of an ERC1155", async () => { 
+        it("Can mint 1 with the transfer of an ERC1155", async () => {
             // Mint 10 tokens to owner by transferring the mock erc1155 to the receivable token
-            await mockERC1155.connect(address1).safeTransferFrom(address1.address, receivable1155.address, 0, 10, "0x");
+            await mockERC1155.connect(address1).safeTransferFrom(address1.address, receivable1155.address, 0, 1, "0x");
 
-            assert.equal((await receivable1155.balanceOf(address1.address)).toString(), 10)
+            assert.equal((await receivable1155.balanceOf(address1.address)).toString(), 1)
         })
-     })
+    })
 
     describe("721 Payments", async () => {
-        before(async () => { 
+        before(async () => {
             // Deploy mock 721 that can be used as payment
             const MockERC721 = await ethers.getContractFactory("MockERC721");
             mockERC721 = await MockERC721.deploy("MockERC721", "M721");
             mockERC721 = await mockERC721.deployed();
-            
+
             // Mint 10 tokens to address 1
             await mockERC721.connect(address1).mint(address1.address, 0);
             assert.equal((await mockERC721.balanceOf(address1.address)).toString(), 1)
@@ -113,7 +113,7 @@ describe("Jackpot", function () {
             receivable721 = await receivable721.deployed();
         })
 
-        it("Can mint 1 with the transfer of an ERC721", async () => { 
+        it("Can mint 1 with the transfer of an ERC721", async () => {
             // Mint 10 tokens to owner by transferring the mock erc721 to the receivable token
             await mockERC721.connect(address1)['safeTransferFrom(address,address,uint256)'](
                 address1.address,
@@ -122,6 +122,47 @@ describe("Jackpot", function () {
             ); // syntax is as such due to overloaded function
 
             assert.equal((await receivable721.balanceOf(address1.address)).toString(), 1)
+        })
+    });
+
+    describe("ERC20 Payments", async () => {
+        before(async () => {
+            // Deploy mock ERC20 that can be used as payment
+            const MockERC20 = await ethers.getContractFactory("MockERC20");
+            mockERC20 = await MockERC20.deploy("MockERC20", "M20");
+            mockERC20 = await mockERC20.deployed();
+
+            // Mint 10 tokens to address 1
+            await mockERC20.connect(address1).mint(address1.address, 10);
+            assert.equal((await mockERC20.balanceOf(address1.address)).toString(), 10)
+
+            // Deploy receivable token
+            receivableERC20 = await ReceivableToken.deploy(
+                "ReceivableToken",
+                "RBT",
+                {
+                    tokenType: 1,
+                    tokenAddress: mockERC20.address,
+                    tokenId: 0,
+                    aux: 1
+                },
+            );
+            receivableERC20 = await receivableERC20.deployed();
+        })
+
+        it("Can mint 1 with the transfer of an ERC20", async () => {
+            // set allowance for erc20 token
+            await mockERC20.connect(address1).approve(receivableERC20.address, 1);
+
+            // Mint 1 token to owner by transferring the mock erc20 to the receivable token
+            await receivableERC20.connect(address1).mintToken({
+                tokenType: 1,
+                tokenAddress: mockERC20.address,
+                tokenId: 0,
+                aux: 1
+            });
+
+            assert.equal((await receivableERC20.balanceOf(address1.address)).toString(), 1)
         })
     });
 });
